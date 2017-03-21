f6e9f3a64c0cd189e411bb835d24b1c8c1ad16bb
pasharik <pasharik@mail.ru>
2017-03-21 17:23:58 +0000

    Listint 1.1

---
+++ src/ru/pasharik/chapter1/Listing1_1.java
@@ -0,0 +1,50 @@
+package ru.pasharik.chapter1;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by pasharik on 21/03/17.
+ */
+public class Listing1_1 {
+    private static Set<Integer> set = new HashSet<Integer>() {
+        @Override
+        public boolean add(Integer integer) {
+            if (contains(integer)) throw new RuntimeException("" + integer);
+            return super.add(integer);
+        }
+    };
+
+    private UnsafeSequence unsafeSequence = new UnsafeSequence();
+
+    @NotThreadSafe
+    private class UnsafeSequence {
+        private int value;
+
+        /** Returns a unique value.*/
+        public int getNext() {
+            return value++;
+        }
+    }
+
+    private class Runner implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) set.add(unsafeSequence.getNext());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Listing1_1().start();
+    }
+
+    private void start() {
+        new Thread(new Runner()).start();
+        new Thread(new Runner()).start();
+        new Thread(new Runner()).start();
+        new Thread(new Runner()).start();
+    }
+
+}
