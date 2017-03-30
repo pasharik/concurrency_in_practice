a32807bce21ec3feb538f39bee6faaaf9b43dd04
Pavel Pozdeev <pasharik@mail.ru>
2017-03-30 19:35:37 +0100

    Listing 3.1 - No visibility

---
+++ src/ru/pasharik/chapter3/Listing3_1/NoVisibility.java
@@ -0,0 +1,24 @@
+package ru.pasharik.chapter3.Listing3_1;
+
+/**
+ * Created by pasharik on 30/03/17.
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        public void run() {
+            while (!ready)
+                Thread.yield();
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+
+}
