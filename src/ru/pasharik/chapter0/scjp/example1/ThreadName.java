589b98004a3185402eabeab5eccc5706e7b44eb3
Pavel Pozdeev <pasharik@mail.ru>
2017-04-17 19:32:21 +0100

    SCJP - thread name

---
+++ src/ru/pasharik/chapter0/scjp/example1/ThreadName.java
@@ -0,0 +1,25 @@
+package ru.pasharik.chapter0.scjp.example1;
+
+/**
+ * Created by pasharik on 17/04/17.
+ */
+public class ThreadName {
+    private static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 4; i++) {
+                try {
+                    System.out.println(i + " " + Thread.currentThread().getName());
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) { }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable r = new MyRunnable();
+        new Thread(r).start();
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}
