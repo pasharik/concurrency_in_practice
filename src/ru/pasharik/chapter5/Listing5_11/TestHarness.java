45e13663ac10af1853143fb4026a3eeb87930c2a
Pavel Pozdeev <pasharik@mail.ru>
2017-04-19 18:53:38 +0100

    Listing 5.11 - Test harness - start threads at the same time with CountDownLatch

---
+++ src/ru/pasharik/chapter5/Listing5_11/TestHarness.java
@@ -0,0 +1,35 @@
+package ru.pasharik.chapter5.Listing5_11;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by pasharik on 19/04/17.
+ */
+public class TestHarness {
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread() {
+                public void run() {
+                    try {
+                        startGate.await();
+                        try {
+                            task.run();
+                        } finally {
+                            endGate.countDown();
+                        }
+                    } catch (InterruptedException ignored) { }
+                }
+            };
+            t.start();
+        }
+
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
