ecb997b98beed4771576d80602076ccaf2e8ef74
Pavel Pozdeev <pasharik@mail.ru>
2017-07-15 15:45:07 +0100

    Stishok-poroshok crawler - using ExecutorCompletionService. Print page when it's ready

--- src/ru/pasharik/chapter0/crawling/developerslife/Crawler.java
+++ src/ru/pasharik/chapter0/crawling/developerslife/Crawler.java
@@ -3,7 +3,6 @@ package ru.pasharik.chapter0.crawling.developerslife;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by pasharik on 10/05/17.
@@ -11,42 +10,38 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Crawler {
     private static final int NUM_THREADS = 4;
     private final ConcurrentMap<Integer, Pirojok> map = new ConcurrentHashMap<>(); //Just to ensure uniqueness
-    private final BlockingQueue<List<Pirojok>> q = new LinkedBlockingQueue<List<Pirojok>>(); //To store pages when ready
-    private final ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
-    private final AtomicInteger pageNum = new AtomicInteger(1);
+    private final ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+    private final ExecutorCompletionService<List<Pirojok>> service = new ExecutorCompletionService<List<Pirojok>>(executor);
     private static final int PAGE_LIMIT = 4;
     private static final int LOWER_RATING = 400;
     private static final int UPPER_RATING = 900;
 
-    private class CrawlerRun implements Runnable {
+    private class CrawlerRun implements Callable<List<Pirojok>> {
+        int pageNum;
+        public CrawlerRun(int pageNum) { this.pageNum = pageNum; }
+
         @Override
-        public void run() {
-            int pNum = pageNum.getAndIncrement();
-            while (pNum <= PAGE_LIMIT) {
-                List<Pirojok> list = Parser.parse(pNum);
-                List<Pirojok> res = new ArrayList<>();
-                for (Pirojok p : list) {
-                    if (map.putIfAbsent(p.getId(), p) == null) res.add(p);
-                }
-                pNum = pageNum.getAndIncrement();
-                q.offer(res);
+        public List<Pirojok> call() throws Exception {
+            List<Pirojok> res = new ArrayList<>();
+            for (Pirojok p : Parser.parse(pageNum)) {
+                if (map.putIfAbsent(p.getId(), p) == null) res.add(p);
             }
+            return res;
         }
     };
 
     public void start() throws ExecutionException, InterruptedException {
-        for (int i = 0; i < NUM_THREADS; i++) {
-            service.submit(new CrawlerRun());
+        for (int pNum = 1; pNum <= PAGE_LIMIT; pNum++) {
+            service.submit(new CrawlerRun(pNum));
         }
-        service.shutdown();
-
-        for (int i = 0; i < PAGE_LIMIT; i++) {
-            for (Pirojok pir : q.take()) {
+        for (int i = 1; i <= PAGE_LIMIT; i++) {
+            for (Pirojok pir : service.take().get()) {
                 if (pir.getRating() > LOWER_RATING && pir.getRating() < UPPER_RATING) {
                     System.out.println(pir);
                 }
             }
         }
+        executor.shutdown();
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
